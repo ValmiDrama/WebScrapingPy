ee5733ed30ffdcecec36a2943938b665a3189d94
ValmiDrama <95919292+ValmiDrama@users.noreply.github.com>
2022-06-02 16:52:46 -0300

    Update teste_1.py

--- teste1/teste_1.py
+++ teste1/teste_1.py
@@ -1,3 +1,17 @@
+#------------------------------------------------------------------------------------------#
+#                              Valmi Gomes Silva Junior
+#------------------------------------------------------------------------------------------#
+# 
+# Este código é parte do processo seletivo, que consiste na realização de testes de 
+# nivelamento básico. 
+# Estes testes foram desenhados como versões simplificadas de tarefas realizadas no dia a 
+# dia da empresa e têm como objetivo avaliar:
+
+# Conhecimentos básicos de programação
+# Organização do código
+# Conhecimentos complementares de boas práticas de programação
+#------------------------------------------------------------------------------------------#
+
 import os
 
 from zipfile import ZipFile
