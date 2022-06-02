75d3d85dc61bc3537cb1ff444c1a86fac14612df
ValmiDrama <95919292+ValmiDrama@users.noreply.github.com>
2022-06-02 16:46:38 -0300

    Teste 2

--- teste2/teste2.py
+++ teste2/teste2.py
@@ -1,15 +1,59 @@
-import tabula
+# Tarefas de código:
+
+# Extrair do pdf do anexo I 
+# Salvar dados em uma tabela em csv;
+# e Zipar o csv num arquivo "Teste_{seu_nome}.zip".
+
+import os
+import pandas as pd
+import tabula as tb
+from tabula import read_pdf
+from zipfile import ZipFile
+from tabula import convert_into
+
+#Funcoes salvas para futuras implementacoes:
+#Funcao que converte para CSV:
+def convert_into_csv(pagina):    
+    tb.convert_into(pdf_path, 'tabela2.csv', output_format='csv', pages=pagina)
+
+#Funcao de coletar uma tabela ate uma pagina espeficica.
+def getTable(dataframes, endPag):    
+    for tabela in range(endPag):
+        dataframes[tabela].to_csv(f"tabela_{tabela}.csv")
+
+#Funcao que converte para ZIP:        
+def convertToZip():    
+    with ZipFile("ValmiJunior.zip", "w") as myzip:
+        myzip.write('tabela.csv')   
+        print('Convertido com sucesso.')
+        myzip.close()
 
 
-pdf_papth = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.pdf'
+# localizacao do arquivo (local/web)
+dir = os.getcwd()
+output_name = '{}\ValmiJunior'.format(dir)
+# pdf_path='https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537.pdf'
+pdf_path='arquivo.pdf'
+table_file = r"arquivo.pdf"
+output_csv = r"/file.csv"
 
-lista_tabelas = tabula.read_pdf("pdf_papth", pages='3')
-print(len(lista_tabelas))
 
+#Verificar arquivo e tamnho (futuramente evitar erros de null e refatoramento)
+listArquivo = tb.read_pdf(pdf_path, pages='all')
+print(len(listArquivo))
 
+# Extrair do pdf do anexo I 
+# dfs = tb.read_pdf(pdf_path, encoding='utf-8', spreadsheet=True, pages='all')
+# print(len(dfs))
 
-# convert PDF into CSV
-tabula.convert_into("test.pdf", "output.csv", output_format="csv", pages='all')
+# Salvar dados em uma tabela em CSV:
+# dfs[0].to_csv("ValmiJunior.csv")
 
-# convert all PDFs in a directory
-tabula.convert_into_by_batch("input_directory", output_format='csv', pages='all')
+tb.convert_into(pdf_path, 'tabela.csv', output_format='csv', pages='all')
+
+# e Zipar o csv num arquivo "Teste_{seu_nome}.zip".
+# convertToZip(seu_nome='ValmiJunior')
+
+convertToZip()
+
+print('fim')
